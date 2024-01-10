e1c5d3320584104dbb4691ed37d4ee46f659128f
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2024-01-10 20:32:58 +0530

    Time: 91 ms (45.66%) | Memory: 76.6 MB (42.03%) - LeetSync

---
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> ans(pref);
+        for(int i=1;i<pref.size();i++){
+            ans[i] = pref[i-1]^pref[i];
+        }
+        return ans;
+    }
+};
