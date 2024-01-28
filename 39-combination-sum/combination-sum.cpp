de0cdc7dac9be3b20bb0d4f8e1699919fe250dd5
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2024-01-28 20:16:26 +0530

    Time: 8 ms (51.66%) | Memory: 12.3 MB (48.06%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void combinationSumHelper(vector<int>& candidates, int target,
+                              vector<vector<int>>& ans, vector<int>& v,
+                              int index) {
+        //   base case
+        if (target == 0) {
+            ans.push_back(v);
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        for (int i = index; i < candidates.size(); i++) {
+            v.push_back(candidates[i]);
+            combinationSumHelper(candidates, target - candidates[i], ans, v, i);
+            v.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        combinationSumHelper(candidates, target, ans, v, 0);
+        return ans;
+    }
+};
