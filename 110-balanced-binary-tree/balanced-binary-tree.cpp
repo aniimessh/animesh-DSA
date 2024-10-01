56bce9055c548d5906917d7742495057f9c54a5f
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2024-10-01 10:36:25 +0530

    Time: 84 ms (5.24%) | Memory: 21.8 MB (71.22%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,13 +6,12 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int height(TreeNode* root) {
+ int height(TreeNode* root) {
         if (root == NULL) {
             return 0;
         }
@@ -21,7 +20,6 @@ public:
         int ans = 1 + max(left, right);
         return ans;
     }
-
     bool isBalanced(TreeNode* root) {
         if (root == NULL) {
             return true;
