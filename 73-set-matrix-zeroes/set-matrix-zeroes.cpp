a226938a88a64e4db198584970f9e7eb15be1240
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2026-01-19 01:13:00 +0530

    Time: 4 ms (5.49%) | Memory: 18.6 MB (78.14%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,61 @@
 class Solution {
 public:
+    // brute force
+    // void markRow(int row, vector<vector<int>>& matrix) {
+    //     int m = matrix[0].size();
+    //     for (int j = 0; j < m; j++) {
+    //         if (matrix[row][j] != 0) {
+    //             matrix[row][j] = -1;
+    //         }
+    //     }
+    // }
+
+    // void markCol(int col, vector<vector<int>>& matrix) {
+    //     int n = matrix.size();
+    //     for (int i = 0; i < n; i++) {
+    //         if (matrix[i][col] != 0) {
+    //             matrix[i][col] = -1;
+    //         }
+    //     }
+    // }
+
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<vector<int>> visited = matrix;
-        // for rows
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+        // for (int i = 0; i < m; i++) {
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (matrix[i][j] == 0) {
+        //             markRow(i, matrix);
+        //             markCol(j, matrix);
+        //         }
+        //     }
+        // }
+
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (matrix[i][j] == -1) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    for (int k = 0; k < m; k++) {
-                        visited[i][k] = 0;
-                    }
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        // for columns;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    for (int k = 0; k < n; k++) {
-                        visited[k][j] = 0;
-                    }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
                 }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = visited[i][j];
             }
         }
     }
