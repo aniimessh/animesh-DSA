77b05ed8ec75394e7ff5636fbe410aeb931b6a20
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2026-01-21 23:56:13 +0530

    Time: 3 ms (20.34%) | Memory: 71.7 MB (51.52%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int maxSumArrayHelper(vector<int>& v, int start, int end) {
-        if (start == end)
-            return v[start];
-        int maxLeft = INT_MIN, maxRIght = INT_MIN;
-        int mid = start + (end - start) / 2;
-
-        int leftSum = maxSumArrayHelper(v, start, mid);
-        int rightum = maxSumArrayHelper(v, mid + 1, end);
-
-        int leftBorderSum = 0, rightBorderSum = 0;
-        for (int i = mid; i >= start; i--) {
-            leftBorderSum += v[i];
-            if (leftBorderSum > maxLeft)
-                maxLeft = leftBorderSum;
-        }
-        for (int i = mid + 1; i <= end; i++) {
-            rightBorderSum += v[i];
-            if (rightBorderSum > maxRIght)
-                maxRIght = rightBorderSum;
-        }
-        int crossBorderSum = maxLeft + maxRIght;
-        return max(leftSum, max(rightum, crossBorderSum));
-    }
-
     int maxSubArray(vector<int>& nums) {
-        return maxSumArrayHelper(nums, 0, nums.size() - 1);
+        int maxi = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
     }
 };
